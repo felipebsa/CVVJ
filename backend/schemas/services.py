ca429072f2331e8398cab7e0027f337627eb428e
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-06-05 15:01:00 -0300

    feat: add ServicesSchemas

---
+++ backend/schemas/services.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class ServicesSchema(BaseModel):
+    vehicle_id: int
+    title: str
+    desc: str
+    date: str
+    labor_value: Optional[float]
+    parts_value: Optional[float]
+    parts_desc: Optional[str]
+    finish: bool
