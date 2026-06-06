4165d6402d5a7559a10b29ed22d5244f3a94103a
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-06-06 11:01:50 -0300

    feat: add routers in main

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from database import Base, engine
 from fastapi import FastAPI
-from routes.vehicle import router
+
+#routers
+from routes.vehicle import router as vehicle_router
+from routes.services import router as service_router
+
+#models
 from models.vehicle import Vehicle
+from models.services import Service
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
-app.include_router(router)
+
+#include routers
+app.include_router(vehicle_router)
+app.include_router(service_router)
