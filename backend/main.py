ef20d2f5a7cadadffe17443cbfb9c1d205e11a1b
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-06-20 14:58:51 -0300

    feat: add images in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,29 @@
 from database import Base, engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 #routers
 from routes.vehicle import router as vehicle_router
 from routes.services import router as service_router
+from routes.vehicle_images import router as image_router
 
 #models
 from models.vehicle import Vehicle
 from models.services import Service
+from models.vehicle_images import Images
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
-#include routers
-app.include_router(vehicle_router)
-app.include_router(service_router)
-
 #settings Cors
-from fastapi.middleware.cors import CORSMiddleware
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+#include routers
+app.include_router(vehicle_router)
+app.include_router(service_router)
+app.include_router(image_router)
