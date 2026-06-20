47f03d91666ce6f0616c26dbebb6bb7a1e423c03
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-06-20 14:56:07 -0300

    feat: add basic routes

--- backend/models/vehicle_images.py
+++ backend/models/vehicle_images.py
@@ -5,6 +5,6 @@ from database import Base
 class Images(Base):
     __tablename__ = "vehicle_images"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    image_id: Mapped[int] = mapped_column(primary_key=True)
     vehicle_id: Mapped[int] = mapped_column(ForeignKey("vehicles.vehicle_id"))
     image_path: Mapped[str] = mapped_column()
---
+++ backend/routes/vehicle_images.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from models.vehicle import Vehicle
+from models.vehicle_images import Images
+from schemas.vehicle_images import SchemaVehicle_Image
+from database import get_db
+
+router = APIRouter()
+
+@router.get("/")
+def images_home():
+    return {"message": "successful home"}
+
+@router.get("/vehicle_images/id/{id}")
+def get_image(id: int, db: Session = Depends(get_db)):
+    query = select(Images).where(Images.image_id==id)
+    db_image = db.execute(query).scalars().first()
+    if db_image is None:
+        raise HTTPException(status_code=404, detail="not found image id")
+    return {"message": db_image}
+
+@router.post("/vehicle_images/register")
+def create_image(images: SchemaVehicle_Image, db: Session = Depends(get_db)):
+    query = select(Vehicle).where(Vehicle.vehicle_id==images.vehicle_id)
+    get_query = db.execute(query).scalars().first()
+    if get_query is None:
+        raise HTTPException(status_code=404, detail="not found vehicle id")
+    db_image = Images(
+        vehicle_id = images.vehicle_id,
+        image_path = images.image_path
+    )
+    db.add(db_image)
+    db.commit()
+    return {"message": "successful create_image"}
